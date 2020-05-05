868b8c837a673516a4652b4aeaa2ab81a509b1f1
youngstone007 <youngstone007@163.com>
2020-05-05 16:16:55 +0800

    new project

---
+++ src/main/java/com/xiaomeng/modeldesign/ModeldesignApplication.java
@@ -0,0 +1,13 @@
+package com.xiaomeng.modeldesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeldesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModeldesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaomeng/modeldesign/ModeldesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomeng.modeldesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeldesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
